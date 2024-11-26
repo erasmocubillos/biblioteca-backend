d2c39a0fcb51bb9bcd80baa35384e64d396be049
erasmocubillos <eerasmocubillos@ucundinamarca.edu.co>
2024-11-26 18:21:34 -0500

    Fix ALLOWED_HOSTS and root URL

--- biblioteca_backend/urls.py
+++ biblioteca_backend/urls.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 from rest_framework.authtoken.views import obtain_auth_token
 
+def root_view(request):
+    return HttpResponse("Bienvenido a la API de Biblioteca Digital")
 
 urlpatterns = [
+    path('', root_view),
     path('admin/', admin.site.urls),
     path('api/', include('libros.urls')),
     path('api/token/', obtain_auth_token, name='obtain-token'),
