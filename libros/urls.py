8a87fc2847a44e0f617be401d2b177f2963418d5
erasmocubillos <eerasmocubillos@ucundinamarca.edu.co>
2024-11-27 13:39:02 -0500

    Updated urls.py

--- libros/urls.py
+++ libros/urls.py
@@ -3,6 +3,7 @@ from .views import CategoriaListCreateView, LibroListView, LibroUploadView, Libr
 
 urlpatterns = [
     path('categorias/', CategoriaListCreateView.as_view(), name='categoria-list-create'),
+    path('categorias/create/', CategoriaListCreateView.as_view(), name='categoria-create'),
     path('libros/', LibroListView.as_view(), name='libro-list'),
     path('libros/upload/', LibroUploadView.as_view(), name='libro-upload'),
     path('libros/<int:pk>/', LibroDetailView.as_view(), name='libro-detail'),
