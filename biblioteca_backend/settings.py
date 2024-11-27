f4b8fa2b40ea66476defa13a556cae839d96436e
erasmocubillos <eerasmocubillos@ucundinamarca.edu.co>
2024-11-27 13:47:59 -0500

    Pagination

--- biblioteca_backend/settings.py
+++ biblioteca_backend/settings.py
@@ -119,6 +119,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 X_FRAME_OPTIONS = "SAMEORIGIN"
